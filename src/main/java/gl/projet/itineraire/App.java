ef38937e74a6fcc21f3cb6ab13c484e11f335e3b
Robin Brumbter <brumbter1u@etu.univ-lorraine.fr>
2023-05-06 15:41:39 +0200

    Creating empty Java Maven project

---
+++ src/main/java/gl/projet/itineraire/App.java
@@ -0,0 +1,7 @@
+package gl.projet.itineraire;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/gl/projet/itineraire/AppTest.java
@@ -0,0 +1,12 @@
+package gl.projet.itineraire;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void myTest() {
+        assert (true);
+    }
+
+}
