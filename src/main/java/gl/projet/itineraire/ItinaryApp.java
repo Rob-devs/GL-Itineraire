e31d9c59b89cf9587cf96384612eabb00f1f94dc
GononEmmanuel <70567754+GononEmmanuel@users.noreply.github.com>
2023-05-15 21:21:44 +0200

    Prompt for getPreferredItinary + return

--- src/main/java/gl/projet/itineraire/ItinaryApp.java
+++ src/main/java/gl/projet/itineraire/ItinaryApp.java
@@ -1,8 +1,15 @@
 package gl.projet.itineraire;
 
+import gl.projet.itineraire.Utils.Constants;
+
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 public class ItinaryApp {
+
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
@@ -17,11 +24,26 @@ public class ItinaryApp {
     }
 
     // TODO : implémenter la méthode
-    // Recherche d'itinéraire favorite entre le plus rapide et le moins d'accident
+    // Recherche d'itinéraire favorite entre le plus rapide et le moins de changement de ligne
     // (regarder Constants.java)
     // MANU
     public String getPreferredItinary() {
-        return null;
+        System.out.println("Veuillez choisir quel type d'itinéraire vous préféré : ");
+        System.out.println("1 - Le plus rapide");
+        System.out.println("2 - Le moins de changement");
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            int choice = scan.nextInt();
+
+            return switch (choice){
+                case 1 -> Constants.ITINARY_FASTEST;
+                case 2 -> Constants.ITINARY_NO_CHANGE;
+                default -> getPreferredItinary();
+            };
+        }catch (InputMismatchException e){
+            return getPreferredItinary();
+        }
     }
 
     // TODO : implémenter la méthode
