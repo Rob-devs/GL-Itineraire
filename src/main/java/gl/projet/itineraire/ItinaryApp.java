8f2ccbcb5c0e0d0f571e90a7d98095c0a5092130
Froliger Robin <robin.froliger@etu.univ-orleans.fr>
2023-06-02 18:35:00 +0200

    Changes in displayPathInfos to split with hh mm ss

--- src/main/java/gl/projet/itineraire/ItinaryApp.java
+++ src/main/java/gl/projet/itineraire/ItinaryApp.java
@@ -463,6 +463,10 @@ public class ItinaryApp {
     public static void displayPathInfos(Path path) {
         System.out.println("Informations sur le chemin\n==================================\n");
         System.out.printf("Temps de trajet : %s \n", path.getTravelTime());
+        int hh = path.getTravelTime()/ 3600;
+        int mm =(path.getTravelTime() % 3600) / 60;
+        int ss = path.getTravelTime() % 60;
+        System.out.printf("Heures : %s \nMinutes : %s \nSecondes : %s \n", hh, mm, ss);
         System.out.printf("Nombre de changement de lignes : %s \n==================================\n", path.getStationChanges());
     }
 
